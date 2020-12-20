9aad11ddd8856d4fd0f32674c4c7f7d76be1e43d
toson77 <t182324@cc.utsunomiya-u.ac.jp>
2020-12-20 18:48:50 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container class="max_height">
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+<style>
+.max_height {
+  height: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="text-center"></div>
+        <v-card>
+          <v-card-text>
+            <h1>
+              VRC is very dark, so be careful... Do you want to look into the
+              abyss?
+            </h1>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="error" nuxt to="/no"> NO </v-btn>
+            <v-btn color="primary" nuxt to="/yes"> YES </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    /* Logo,
+    VuetifyLogo, */
+  },
+}
+</script>
+
+<style>
+h1 {
+  font-size: 50px;
+  line-height: 1.4em;
+}
+</style>
