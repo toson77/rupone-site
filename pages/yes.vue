f91777d6a934daa46b9c3db3e5c1db014fbf009d
toson77 <t182324@cc.utsunomiya-u.ac.jp>
2020-12-20 20:47:14 +0900

    fin

--- pages/yes.vue
+++ pages/yes.vue
@@ -1,15 +1,37 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="text-center"></div>
+        <v-card>
+          <v-card-text>
+            <h1>You must not enter the VRC! Go to the cluster!</h1>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="error" href="https://cluster.mu/"> Go </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    /* Logo,
+    VuetifyLogo, */
+  },
+}
+</script>
+
+<style>
+h1 {
+  font-size: 50px;
+  line-height: 1.4em;
+}
+</style>
